bc40e06168781ccd7b9ed0825a3f46ba2ba30537
Vincent White <xstrengthofonex@gmail.com>
2020-01-26 15:57:22 +0900

    created onCriteria func

--- internal/co_coders/match/on_collab_style_test.go
+++ internal/co_coders/match/on_collab_style_test.go
@@ -39,3 +39,43 @@ func TestMatchOnCollabStyleSubsetReturnsAMatch(t *testing.T) {
 	assert.Equal(t, []user.User{aUser}, actual)
 }
 
+func TestMatchHasAScore(t *testing.T) {
+	aCriteria := user.New([]criteria.CollabStyle{criteria.Team, criteria.Pair})
+	users := []user.User{aCriteria}
+	aMatch := OnCriteria(aCriteria, users)
+	assert.Equal(t,1.0, aMatch.Score)
+}
+
+func TestMatchHasAScoreOfZeroWithNoMatch(t *testing.T) {
+	matchCriteria := user.New([]criteria.CollabStyle{criteria.Pair})
+	badMatch := user.New([]criteria.CollabStyle{criteria.Team})
+	users := []user.User{badMatch}
+	aMatch := OnCriteria(matchCriteria, users)
+	assert.Equal(t,0.0, aMatch.Score)
+}
+
+func TestMatchScore50Percent(t *testing.T) {
+	// TODO name does not match type user to criteria
+	matchCriteria := user.New([]criteria.CollabStyle{criteria.Pair, criteria.Mob})
+	halfMatch := user.New([]criteria.CollabStyle{criteria.Mob})
+	users := []user.User{halfMatch}
+	aMatch := OnCriteria(matchCriteria, users)
+	assert.Equal(t,0.5, aMatch.Score)
+}
+
+type Match struct{
+	Score float64
+}
+
+func OnCriteria(aCriteria user.User, users []user.User) Match {
+	total := 0.0
+	matches := 0.0
+	for _, collabStyle := range aCriteria.CollabStyles {
+		result := match.OnCollabStyle(users, collabStyle)
+		if len(result) > 0 {
+			matches ++
+		}
+		total ++
+	}
+	return Match{matches / total}
+}
