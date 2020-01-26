36b068617807b4aa0618276243a38227f761ddf8
Vincent White <xstrengthofonex@gmail.com>
2020-01-26 16:35:00 +0900

    Refactored user type to criteria type in onCriteria tests

--- internal/co_coders/match/on_collab_style_test.go
+++ internal/co_coders/match/on_collab_style_test.go
@@ -40,34 +40,33 @@ func TestMatchOnCollabStyleSubsetReturnsAMatch(t *testing.T) {
 }
 
 func TestMatchHasAScore(t *testing.T) {
-	aCriteria := user.New([]criteria.CollabStyle{criteria.Team, criteria.Pair})
-	users := []user.User{aCriteria}
-	aMatch := OnCriteria(aCriteria, users)
+	matchCriteria := criteria.Criteria{
+		CollabStyles: []criteria.CollabStyle{criteria.Team}}
+	allMatchUser := user.New(matchCriteria.CollabStyles)
+	usersToMatch := []user.User{allMatchUser}
+	aMatch := OnCriteria(matchCriteria, usersToMatch)
 	assert.Equal(t,1.0, aMatch.Score)
 }
 
 func TestMatchHasAScoreOfZeroWithNoMatch(t *testing.T) {
-	matchCriteria := user.New([]criteria.CollabStyle{criteria.Pair})
-	badMatch := user.New([]criteria.CollabStyle{criteria.Team})
-	users := []user.User{badMatch}
-	aMatch := OnCriteria(matchCriteria, users)
+	matchCriteria := criteria.Criteria{
+		CollabStyles: []criteria.CollabStyle{criteria.Pair}}
+	noMatchUser := user.New([]criteria.CollabStyle{criteria.Team})
+	usersToMatch := []user.User{noMatchUser}
+	aMatch := OnCriteria(matchCriteria, usersToMatch)
 	assert.Equal(t,0.0, aMatch.Score)
 }
 
 func TestMatchScore50Percent(t *testing.T) {
-	// TODO name does not match type user to criteria
-	matchCriteria := user.New([]criteria.CollabStyle{criteria.Pair, criteria.Mob})
-	halfMatch := user.New([]criteria.CollabStyle{criteria.Mob})
-	users := []user.User{halfMatch}
-	aMatch := OnCriteria(matchCriteria, users)
+	matchCriteria := criteria.Criteria{
+		CollabStyles: []criteria.CollabStyle{criteria.Pair, criteria.Mob}}
+	halfMatchUser := user.New([]criteria.CollabStyle{criteria.Mob})
+	usersToMatch := []user.User{halfMatchUser}
+	aMatch := OnCriteria(matchCriteria, usersToMatch)
 	assert.Equal(t,0.5, aMatch.Score)
 }
 
-type Match struct{
-	Score float64
-}
-
-func OnCriteria(aCriteria user.User, users []user.User) Match {
+func OnCriteria(aCriteria criteria.Criteria, users []user.User) criteria.Match {
 	total := 0.0
 	matches := 0.0
 	for _, collabStyle := range aCriteria.CollabStyles {
@@ -77,5 +76,5 @@ func OnCriteria(aCriteria user.User, users []user.User) Match {
 		}
 		total ++
 	}
-	return Match{matches / total}
+	return criteria.Match{Score: matches / total}
 }
